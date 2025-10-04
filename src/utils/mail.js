2d98d396fb7bd9d2d6ad3b8c31a17e9b3103244b
ash469 <ashshandilya4@gmail.com>
2025-10-04 20:34:22 +0530

    battle flow done

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async ({ to, subject, html, from = process.env.EMAIL_FROM }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: +process.env.SMTP_PORT,
+    port: process.env.SMTP_PORT,
     secure: process.env.SMTP_SECURE === "true",
     auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
      tls: { rejectUnauthorized: false } // allow self-signed certs
