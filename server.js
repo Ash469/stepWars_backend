3d663d16b479ff5f71d7d0eef957e08285c3197a
ash469 <ashshandilya4@gmail.com>
2025-10-14 20:19:30 +0530

    testing for pvp matching

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-  ensureDummyPlayerExists(); 
+  // ensureDummyPlayerExists(); 
 });
 
-//for testing PVP battles with a dummy player
-const ensureDummyPlayerExists = async () => {
-  const rtdb = admin.database();
-  const dummyPlayerRef = rtdb.ref('matchmakingPool/dummy_player_test_01');
+// //for testing PVP battles with a dummy player
+// const ensureDummyPlayerExists = async () => {
+//   const rtdb = admin.database();
+//   const dummyPlayerRef = rtdb.ref('matchmakingPool/dummy_player_test_01');
   
-  const dummyPlayerData = {
-    uid: 'dummy_player_test_01',
-    username: 'Test Opponent',
-    status: 'waiting',
-    entryTime: 1
-  };  
+//   const dummyPlayerData = {
+//     uid: 'dummy_player_test_01',
+//     username: 'Test Opponent',
+//     status: 'waiting',
+//     entryTime: 1
+//   };  
 
-  await dummyPlayerRef.set(dummyPlayerData);
-  console.log("✔ Dummy player for testing is now available in the matchmaking pool.");
-};
+//   await dummyPlayerRef.set(dummyPlayerData);
+//   console.log("✔ Dummy player for testing is now available in the matchmaking pool.");
+// };
