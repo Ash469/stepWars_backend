1e47ac123698111cfce2e79b9d6e9a7000d75c8e
ash469 <ashshandilya4@gmail.com>
2025-12-16 22:32:37 +0530

    update port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+ app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on port ${PORT}`));
   // ensureDummyPlayerExists(); 
 });
 
